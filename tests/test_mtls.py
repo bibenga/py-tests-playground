31095928aa5668e7f777c5eb0d04017da62bb579
bibenga <{ID}+{username}@users.noreply.github.com>
2023-08-17 16:50:59 +0000

    update ssl tests and asyncio cfg

--- tests/test_mtls.py
+++ tests/test_mtls.py
@@ -1,5 +1,6 @@
 """ An simple example of mtls testing """
 
+from dataclasses import dataclass
 import ssl
 import pytest
 import trustme
@@ -19,44 +20,61 @@ async def secured(request: web.Request) -> web.Response:
     return web.Response(status=428)
 
 
-async def get_client(aiohttp_client, aiohttp_server, verify_mode):
-    """ create client and client ssl context """
+@pytest.fixture
+async def app() -> web.Application:
+    app = web.Application()
+    app.router.add_get('/public', public)
+    app.router.add_get('/secured', secured)
+    return app
+
+
+@dataclass
+class SslTestCfg:
+    ca: trustme.CA
+    server_cert: trustme.LeafCert
+    client_cert: trustme.LeafCert
+    server_ssl: ssl.SSLContext
+    anon_ssl: ssl.SSLContext
+    user_ssl: ssl.SSLContext
+
+
+@pytest.fixture
+def ssl_cfg() -> SslTestCfg:
     ca = trustme.CA()
     server_cert = ca.issue_cert("127.0.0.1", "localhost")
     client_cert = ca.issue_cert("client@127.0.0.1")
 
-    # create server cert
-    server_ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ca.configure_trust(server_ssl_context)
-    server_cert.configure_cert(server_ssl_context)
-    server_ssl_context.verify_mode = verify_mode
+    server_ssl = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ca.configure_trust(server_ssl)
+    server_cert.configure_cert(server_ssl)
+    # server_ssl.verify_mode = verify_mode
 
     # create client cert
-    client_anonymous_ssl_context = ssl.create_default_context()
-    ca.configure_trust(client_anonymous_ssl_context)
+    anon_ssl = ssl.create_default_context()
+    ca.configure_trust(anon_ssl)
 
     # create client cert
-    client_ssl_context = ssl.create_default_context()
-    ca.configure_trust(client_ssl_context)
-    client_cert.configure_cert(client_ssl_context)
+    user_ssl = ssl.create_default_context()
+    ca.configure_trust(user_ssl)
+    client_cert.configure_cert(user_ssl)
 
-    # init web application
-    app = web.Application()
-    app.router.add_get('/public', public)
-    app.router.add_get('/secured', secured)
-
-    # create http client
-    server = await aiohttp_server(app, ssl=server_ssl_context)
-    client = await aiohttp_client(server)
-
-    return client, client_ssl_context, client_anonymous_ssl_context
+    return SslTestCfg(
+        ca=ca,
+        server_cert=server_cert,
+        client_cert=client_cert,
+        server_ssl=server_ssl,
+        anon_ssl=anon_ssl,
+        user_ssl=user_ssl,
+    )
 
 
-@pytest.mark.asyncio
-async def test_https(aiohttp_client, aiohttp_server, loop):
+async def test_https(ssl_cfg: SslTestCfg, app: web.Application, aiohttp_client, aiohttp_server) -> None:
     """test https server without client cert"""
 
-    client, _, _ = await get_client(aiohttp_client, aiohttp_server, ssl.CERT_NONE)
+    # create http client
+    ssl_cfg.server_ssl.verify_mode = ssl.CERT_NONE
+    server = await aiohttp_server(app, ssl=ssl_cfg.server_ssl)
+    client = await aiohttp_client(server)
 
     # connect with cert validation
     try:
@@ -65,18 +83,24 @@ async def test_https(aiohttp_client, aiohttp_server, loop):
     except ClientConnectorCertificateError:
         pass
 
+    # connect with ca
+    resp = await client.get('/public', ssl=ssl_cfg.anon_ssl)
+    assert resp.status == 428
+
     # connect without cert validation
     resp = await client.get('/public', verify_ssl=False)
     assert resp.status == 428
 
 
-@pytest.mark.asyncio
-async def test_mtls_required(aiohttp_client, aiohttp_server, loop):
+async def test_mtls_required(ssl_cfg: SslTestCfg, app: web.Application, aiohttp_client, aiohttp_server) -> None:
     """ test https server with client cert and server require certeficate """
 
-    client, client_ssl_context, _ = await get_client(aiohttp_client, aiohttp_server, ssl.CERT_REQUIRED)
+    # create http client
+    ssl_cfg.server_ssl.verify_mode = ssl.CERT_REQUIRED
+    server = await aiohttp_server(app, ssl=ssl_cfg.server_ssl)
+    client = await aiohttp_client(server)
 
-    # connect without client cert but with cert validation 
+    # connect without client cert but with cert validation
     try:
         await client.get('/secured', verify_ssl=True)
         assert "where is an error?"
@@ -84,7 +108,7 @@ async def test_mtls_required(aiohttp_client, aiohttp_server, loop):
         # a connection should failed
         pass
 
-    # connect without client cert but without cert validation 
+    # connect without client cert but without cert validation
     try:
         await client.get('/secured', verify_ssl=False)
         assert "where is an error?"
@@ -93,21 +117,21 @@ async def test_mtls_required(aiohttp_client, aiohttp_server, loop):
         pass
 
     # connect with client cert
-    resp = await client.get('/secured', ssl=client_ssl_context)
+    resp = await client.get('/secured', ssl=ssl_cfg.user_ssl)
     assert resp.status == 428
 
-@pytest.mark.asyncio
-async def test_mtls_optional(aiohttp_client, aiohttp_server, loop):
+
+async def test_mtls_optional(ssl_cfg: SslTestCfg, app: web.Application, aiohttp_client, aiohttp_server) -> None:
     """ test https server with client cert and server can accept certeficate """
 
-    client, client_ssl_context, client_anonymous_ssl_context = await get_client(aiohttp_client, aiohttp_server, ssl.CERT_OPTIONAL)
+    ssl_cfg.server_ssl.verify_mode = ssl.CERT_OPTIONAL
+    server = await aiohttp_server(app, ssl=ssl_cfg.server_ssl)
+    client = await aiohttp_client(server)
 
     # connect as anonymous client
-    resp = await client.get('/secured', ssl=client_anonymous_ssl_context)
+    resp = await client.get('/secured', ssl=ssl_cfg.anon_ssl)
     assert resp.status == 403
 
     # connect as user with valid cert
-    resp = await client.get('/secured', ssl=client_ssl_context)
+    resp = await client.get('/secured', ssl=ssl_cfg.user_ssl)
     assert resp.status == 428
-
-
